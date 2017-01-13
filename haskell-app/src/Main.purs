9b9d0a17de1bd1c92ba296368ffc25e9a209ce27
chrislample <clample@crimson.ua.edu>
2017-01-13 17:56:00 -0500

    Get input field working

--- haskell-app/src/Main.purs
+++ haskell-app/src/Main.purs
@@ -1,25 +1,25 @@
 module Main where
 
 import Prelude
-
-import ReactDOM as RDOM
 import React as R
 import React.DOM as R
 import React.DOM.Props as RP
-
+import ReactDOM as RDOM
 import Thermite as T
 import Control.Monad.Eff (Eff)
+import DOM.Event.EventPhase (EventPhase(..))
+import Thermite (defaultPerformAction)
 
-data Action = OverviewClicked | ReceiveFundsClicked
+data Action = OverviewClicked | ReceiveFundsClicked | RequestFunds | SetEditText String
 
 data Context =
   Overview |
   RecieveFunds
 
-type State = { context :: Context }
+type State = { context :: Context, valueText :: String }
 
 initialState :: State
-initialState = { context: Overview }
+initialState = { context: Overview, valueText: "" }
 
 render :: T.Render State _ Action
 render dispatch _ state _ =
@@ -29,17 +29,28 @@ render dispatch _ state _ =
          , R.button [ RP.onClick \_ -> dispatch ReceiveFundsClicked ]
                     [ R.text "ReceiveFunds" ]
         ]
-  , mainScreen state
+  , mainScreen dispatch state
   ]
 
-mainScreen { context: Overview } =
+mainScreen dispatch { context: Overview } =
   R.p' [ R.text "overview page" ]
-mainScreen { context: RecieveFunds } =
-  R.p' [ R.text "RecieveFunds page" ]
+mainScreen dispatch { context: RecieveFunds, valueText } =
+  R.div'
+  [ R.p'      [ R.text "RecieveFunds page" ]
+  , R.input   [ RP.className "form-control"
+              , RP.placeholder "Create a new task"
+              , RP.value valueText
+              , RP.onKeyPress \e -> dispatch (SetEditText e.key)
+              ] []
+  , R.button  [ RP.onClick \_ -> dispatch RequestFunds ]
+              [ R.text "RequestFunds" ]
+  ]
 
 performAction :: T.PerformAction _ State _ Action
 performAction OverviewClicked _ _ = void (T.cotransform (\state -> state { context = Overview}))
 performAction ReceiveFundsClicked _ _ = void (T.cotransform (\state -> state { context = RecieveFunds }))
+performAction RequestFunds _ _ = void (T.cotransform (\state -> state))
+performAction (SetEditText s) _ _ = void (T.modifyState (\state -> state { valueText = state.valueText <> s }))
 
 spec :: T.Spec _ State _ Action
 spec = T.simpleSpec performAction render
