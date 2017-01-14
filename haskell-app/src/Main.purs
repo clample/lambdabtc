057dd1882caca7f6660a69ec936bf24e4be2d564
chrislample <clample@crimson.ua.edu>
2017-01-14 16:10:02 -0500

    Get context from menu

--- haskell-app/src/Main.purs
+++ haskell-app/src/Main.purs
@@ -6,6 +6,7 @@ import Halogen
 import Halogen.HTML.Events.Indexed as E
 import Halogen.HTML.Indexed as H
 import Control.Monad.Eff (Eff)
+import DOM.HTML.HTMLElement (offsetHeight)
 import Halogen.Util (awaitBody, runHalogenAff)
 
 type State =
@@ -16,7 +17,7 @@ data Context =
   RecieveFunds
 
 data Query a =
-  ToggleContext a |
+  ToggleContext Context a |
   GetContext (Context -> a)
 
 initialState :: State
@@ -31,17 +32,19 @@ mainComponent = component { render, eval }
         [ H.h1_
             [ H.text "LamdaBTC" ]
         , H.button
-            [ E.onClick (E.input_ ToggleContext) ]
-            [ H.text (case state.context of
-                        Overview -> "Great"
-                        RecieveFunds -> "Also Great") ]
+            [ E.onClick (E.input_ (ToggleContext Overview)) ]
+            [ H.text "Overview" ]
+        , H.button
+            [ E.onClick (E.input_ (ToggleContext RecieveFunds)) ]
+            [ H.text "Recieve Funds"]
+        , H.p_ [ H.text case state.context of
+                  Overview -> "Overview"
+                  RecieveFunds -> "Recieve Funds"]
         ]
 
     eval :: Query ~> ComponentDSL State Query g
-    eval (ToggleContext next) = do
-      modify (\state -> { context: case state.context of
-                          Overview -> RecieveFunds
-                          RecieveFunds -> Overview })
+    eval (ToggleContext ct next) = do
+      modify (\state -> { context: ct })
       pure next
     eval (GetContext continue) = do
       context <- gets _.context
@@ -51,100 +54,3 @@ main :: Eff (HalogenEffects ()) Unit
 main = runHalogenAff do
   body <- awaitBody
   runUI mainComponent initialState body
-
-{--
-import Prelude
-import React as R
-import React.DOM as R
-import React.DOM.Props as RP
-import ReactDOM as RDOM
-import Thermite as T
-import Control.Monad.Eff (Eff)
-import DOM.Event.EventPhase (EventPhase(..))
-import Thermite (defaultPerformAction)
-
-data Action =
-  OverviewClicked |
-  ReceiveFundsClicked |
-  RequestFunds |
-  SetLabelText String |
-  SetAmountText String |
-  SetMessageText String
-
-data Context =
-  Overview |
-  RecieveFunds
-
-type State = {
-  context :: Context,
-  labelText :: String,
-  amountText :: String,
-  messageText :: String
-  }
-
-initialState :: State
-initialState = {
-  context: Overview,
-  labelText: "",
-  amountText: "",
-  messageText: ""
-  }
-
-render :: T.Render State _ Action
-render dispatch _ state _ =
-  [ R.p' [ R.text "Welcome to LamdaBTC" ]
-  , R.p' [ R.button [ RP.onClick \_ -> dispatch OverviewClicked ]
-                    [ R.text "Overview" ]
-         , R.button [ RP.onClick \_ -> dispatch ReceiveFundsClicked ]
-                    [ R.text "ReceiveFunds" ]
-        ]
-  , mainScreen dispatch state
-  ]
-
-mainScreen dispatch { context: Overview } =
-  R.p' [ R.text "overview page" ]
-mainScreen dispatch { context: RecieveFunds, labelText, amountText, messageText } =
-  R.div'
-  [ R.p'      [ R.text "RecieveFunds page" ]
-  , R.p'      [ R.text "Label:"]
-  , R.input   [ RP.className "form-control"
-              , RP.placeholder "Label"
-              , RP.value labelText
-              , RP.onKeyPress \e -> dispatch (SetLabelText e.key)
-              ] []
-  , R.p'      [ R.text "Amount:"]
-  , R.input   [ RP.className "form-control"
-              , RP.placeholder "Request amount"
-              , RP.value amountText
-              , RP.onKeyPress \e -> dispatch (SetAmountText e.key)
-              ] []
-  , R.select  []
-              [ R.option' [ R.text "BTC" ]
-              , R.option' [ R.text "mBTC"]
-              , R.option' [ R.text "μBTC"]]
-  , R.p'      [ R.text "Message:"]
-  , R.input   [ RP.className "form-control"
-              , RP.placeholder "Message"
-              , RP.value messageText
-              , RP.onKeyPress \e -> dispatch (SetMessageText e.key)
-              ] []
-  , R.button  [ RP.onClick \_ -> dispatch RequestFunds ]
-              [ R.text "RequestFunds" ]
-  ]
-
-performAction :: T.PerformAction _ State _ Action
-performAction OverviewClicked _ _ = void (T.cotransform (\state -> state { context = Overview}))
-performAction ReceiveFundsClicked _ _ = void (T.cotransform (\state -> state { context = RecieveFunds }))
-performAction RequestFunds _ _ = void (T.cotransform (\state -> state))
-performAction (SetLabelText s) _ _ = void (T.modifyState (\state -> state { labelText = state.labelText <> s }))
-performAction (SetMessageText s) _ _ = void (T.modifyState (\state -> state { messageText = state.messageText <> s }))
-
--- TODO: Only accept numbers for this input form
-performAction (SetAmountText s) _ _ = void (T.modifyState (\state -> state { amountText = state.amountText <> s }))
-
-
-spec :: T.Spec _ State _ Action
-spec = T.simpleSpec performAction render
-
-main = T.defaultMain spec initialState unit
---}
