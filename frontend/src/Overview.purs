dfd0a5adf06369a47b7b7bf7f6631b8925720678
adamrk <ark.email@gmail.com>
2017-04-04 10:31:39 -0400

    save Overview state and print all messages received

--- frontend/src/Overview.purs
+++ frontend/src/Overview.purs
@@ -5,11 +5,12 @@ import Prelude
 import Halogen as H
 import Halogen.HTML as HH
 import Data.Maybe (Maybe(..))
+import Data.Array (concatMap, cons)
 
-type OverviewState = { totalFundsMessage :: String }
+type OverviewState = { totalFundsMessages :: Array String }
 
-initialState :: Unit -> OverviewState
-initialState = pure { totalFundsMessage: "no message recieved" }
+initialState :: Array String -> OverviewState
+initialState msgs = { totalFundsMessages: msgs }
 
 data OverviewQuery a
   = IncomingFunds String a
@@ -18,19 +19,32 @@ data OverviewSlot = OverviewSlot
 derive instance eqOverviewSlot :: Eq OverviewSlot
 derive instance ordOverviewSlot :: Ord OverviewSlot
 
-overviewComponent :: forall m. H.Component HH.HTML OverviewQuery Unit Void m
+overviewComponent :: forall m. H.Component HH.HTML OverviewQuery (Array String) Void m
 overviewComponent = H.component { render, eval, initialState, receiver }
   where
 
-  receiver :: forall a. Unit -> Maybe a
+  receiver :: forall a. (Array String) -> Maybe a
   receiver _ = Nothing
 
   render :: OverviewState -> H.ComponentHTML OverviewQuery
   render (state) = HH.div_
     [ HH.h1_ [ HH.text "Overview" ]
-    , HH.p_ [ HH.text state.totalFundsMessage]]
+    , HH.p_ (renderMessages state.totalFundsMessages)
+    ]
+    --, HH.p_ [ HH.text state.totalFundsMessage]]
 
   eval :: OverviewQuery ~> H.ComponentDSL OverviewState OverviewQuery Void m
   eval (IncomingFunds msg next) = do
-    H.modify (_ { totalFundsMessage = msg })
+    prevMessages <- H.gets (\s -> s.totalFundsMessages)
+    H.modify (\s -> s {totalFundsMessages = (cons msg prevMessages) })
     pure next
+  -- eval (GetOverviewState reply) = do
+  --   b <- H.gets (\s -> s.totalFundsMessages)
+  --   pure (reply b)
+
+renderMessages :: Array String -> Array (H.ComponentHTML OverviewQuery)
+renderMessages [] = [HH.text "No new messages."]
+renderMessages xs = concatMap renderMessage xs
+
+renderMessage :: String -> Array (H.ComponentHTML OverviewQuery)
+renderMessage str = [HH.text str, HH.br_]
